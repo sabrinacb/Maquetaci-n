c86db15fcfbdf77b604092aa79ea08c15d8438b0
sabrinacb <79062119+sabrinacb@users.noreply.github.com>
2022-09-15 14:04:13 -0300

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,35 @@
+function abrir(){
+
+
+document.getElementById('desplegable').style.display = "flex"
+
+
+}
+
+
+function cerrar(){
+
+
+    document.getElementById('desplegable').style.display = "none"
+    
+    
+    }
+
+
+    function abrirModal(){
+
+
+        document.getElementById('modal').style.display = "flex"
+        
+        
+        }
+        
+        
+        function cerrarModal(){
+        
+        
+            document.getElementById('modal').style.display = "none"
+            
+            
+            }
+           
